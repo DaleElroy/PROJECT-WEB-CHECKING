5e90ce7761705a91720a36160e67f4111da92e7d
Dale <markdalebagui@gmail.com>
2024-01-17 12:59:51 +0800

    Bago

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,72 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+  @section('content')
+  <section class="vh-100" style="background-color: #ffffff;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-ml-5">
+          <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+              <div class="col-md-6 col-lg-5 d-none d-md-block">
+                  <img src="/img/BeadsLog.png"
+                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height: 700px;" />
+              </div>
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-@section('content')
-<section class="vh-100" style="background-color: #ffffff;">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-ml-5">
-        <div class="card" style="border-radius: 1rem;">
-          <div class="row g-0">
-            <div class="col-md-6 col-lg-5 d-none d-md-block">
-                <img src="/img/BeadsLog.png"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height: 700px;" />
-            </div>
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="card-body p-4 p-lg-5 text-black">
+                  <form method="POST" action="{{ route('register') }}">
+                    @csrf
 
-                <form method="POST" action="{{ route('register') }}">
-                  @csrf
-
-                  <div class="d-flex align-items-center justify-content-center mb-3 pb-1">
-                    <img src="/img/Bilibeads-logo.png" alt="Bilibeads Logo" class="h1 fw-bold mb-0" style="width: 170px; height: 80px;">
-                </div>
-
-                  <!-- Name -->
-                  <div>
-                    <x-input-label for="name" :value="__('Name')" />
-                    <x-text-input id="name" class="form-control form-control-lg" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    <div class="d-flex align-items-center justify-content-center mb-3 pb-1">
+                      <img src="/img/Bilibeads-logo.png" alt="Bilibeads Logo" class="h1 fw-bold mb-0" style="width: 170px; height: 80px;">
                   </div>
 
-                  <!-- Email Address -->
-                  <div class="mt-4">
-                    <x-input-label for="email" :value="__('Email')" />
-                    <x-text-input id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                  </div>
-
-                  <!-- Password -->
-                  <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
-                    <x-text-input id="password" class="form-control form-control-lg" type="password" name="password" required autocomplete="new-password" />
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                  </div>
-
-                  <!-- Confirm Password -->
-                  <div class="mt-4">
-                    <div class="mb-4">
-                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-                        <x-text-input id="password_confirmation" class="form-control form-control-lg" type="password" name="password_confirmation" required autocomplete="new-password" />
-                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    <!-- Name -->
+                    <div>
+                      <x-input-label for="name" :value="__('Name')" />
+                      <x-text-input id="name" class="form-control form-control-lg" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                      <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
-                    <!-- <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                      {{ __('Already registered?') }}
-                    </a> -->
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                      <x-input-label for="email" :value="__('Email')" />
+                      <x-text-input id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                      <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
 
-                
-                    <x-primary-button class="mb-3 btn btn-primary">
-                        {{ __('Register') }}
-                    </x-primary-button>
-                </form>
+                    <!-- Password -->
+                    <div class="mt-4">
+                      <x-input-label for="password" :value="__('Password')" />
+                      <x-text-input id="password" class="form-control form-control-lg" type="password" name="password" required autocomplete="new-password" />
+                      <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
 
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                      <div class="mb-4">
+                          <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                          <x-text-input id="password_confirmation" class="form-control form-control-lg" type="password" name="password_confirmation" required autocomplete="new-password" />
+                          <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                      </div>
+
+                      <!-- <div class="flex items-center justify-end mt-4">
+                      <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                      </a> -->
+
+                  
+                      <x-primary-button class="mb-3 btn btn-primary">
+                          {{ __('Register') }}
+                      </x-primary-button>
+                  </form>
+
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
 
